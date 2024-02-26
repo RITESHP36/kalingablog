6890086d8c1d9c637421c12ace0a9fa4bd6caa8b
RITESHP36 <codingritesh36@gmail.com>
2024-02-26 08:48:23 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ const connectDB = async () => {
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors({ origin: "https://kj-blogging-blitz.vercel.app/", credentials: true }));
+app.use(cors({ origin: "https://kj-blogging-blitz.vercel.app", credentials: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
