00d049f8a5347c1ad130c5ed09b285f256e24e9d
RITESHP36 <codingritesh36@gmail.com>
2024-02-26 08:08:50 +0530

    updated cors in index.js

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ const connectDB = async () => {
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors({ origin: ["http://localhost:5173","*"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","https://kj-blogging-blitz.vercel.app/","*"], credentials: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
